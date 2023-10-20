4ac17aa34273922e4ff1dd374f8fec21987523bf
Ceciliapradob <ceciliapradob0799@gmail.com>
2023-10-20 14:55:16 -0300

    Ejercicios creados por Ceci

---
+++ app.js
@@ -0,0 +1,66 @@
+// //---------------------------------CONDICIONALES--------------------------------
+// Ejercicio 1: Clasificación de números
+
+// Escribe un programa que clasifique un número en una de las siguientes categorías:
+// positivo, negativo, par o impar.El número se debe definir en la variable numero y 
+// tu programa debe imprimir todas las categorías que apliquen al número.
+// Envía dichos valores a un Array.
+
+
+//     Ejercicio 2: Evaluación de condiciones
+
+// Crea un programa que determine si una persona puede obtener una licencia de conducir.
+// Debes definir tres variables: edad, tienePermiso y tieneAntecedentesPenales.
+// Utiliza condicionales y operadores lógicos para evaluar si la persona cumple con los requisitos.
+
+
+//     Ejercicio 3: Evaluación de aprobación en varias asignaturas
+
+// Crea un programa que evalúe si un estudiante ha aprobado una serie de asignaturas.Debes 
+// definir variables para representar las calificaciones en diferentes asignaturas y usar 
+// operadores lógicos para determinar si el estudiante ha aprobado todas las asignaturas.
+
+
+//     //----------------------------------BUCLES------------------------------------------
+//     Ejercicio 1: Suma de números primos
+
+// Escribe un programa que sume todos los números primos dentro de un rango especificado.
+// Debes definir un rango de números y utilizar un bucle for para verificar si cada número es primo y,
+//     en caso afirmativo, sumarlo.
+
+
+//         Ejercicio 2: Generación de patrones
+
+// Escribe un programa que genere un patrón de números, como el siguiente:
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+// Ejercicio 3: Búsqueda en una matriz
+
+// De un array de arrays busca un valor específico.
+// Debes definir una matriz y un valor a buscar, y luego utilizar dos bucles for 
+// para buscar el valor y mostrar su posición en la matriz.
+
+
+//     //------------------------FUNCIONES------------------------------------------
+//     Ejercicio 1: Números primos
+
+// Escribe una función llamada esPrimo que tome un número como argumento y determine si
+// ese número es primo o no.La función debe devolver true si el número es primo y false si no lo es.
+
+
+//     Ejercicio 2: Vocales
+// Crea una función llamada contarVocales que tome una cadena de texto como argumento y 
+// cuente cuántas vocales(a, e, i, o, u) contiene la cadena.La función debe devolver 
+// el número de vocales encontradas.
+
+
+//     Ejercicio 4: Palíndromo
+
+// Crea una función llamada esPalindromo que tome una cadena de texto como argumento y 
+// determine si es un palíndromo.Un palíndromo es una palabra, frase o secuencia que 
+// se lee igual de izquierda a derecha que de derecha a izquierda, sin considerar espacios
+// ni signos de puntuación.La función debe devolver true si la cadena es un palíndromo y false si no lo es.
